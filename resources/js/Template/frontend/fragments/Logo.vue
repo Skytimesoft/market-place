4834eb25236894fe1c341bed608ead64b31377b8
Muhibbullah Ansary <muhibbullah611@gmail.com>
2024-01-24 02:32:53 +0600

    minor change

--- resources/js/Template/frontend/fragments/Logo.vue
+++ resources/js/Template/frontend/fragments/Logo.vue
@@ -2,11 +2,15 @@
     <div>
         <div v-if="footer">
             <Img 
+                class="w-[150px] grayscale"
                 src="https://nimd.org/wp-content/uploads/2019/03/DEMO-Logo-Final.png"
             />
         </div>
         <div v-else>
-            Header logo
+            <Img 
+                class="w-[150px]"
+                src="https://nimd.org/wp-content/uploads/2019/03/DEMO-Logo-Final.png"
+            />
         </div>
     </div>
 </template>
