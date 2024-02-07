ffa78c4cdf63666ea7d8cf14f64f6c91784915e9
Khalid-Hossain-Tarif <khalidhossain1311@gmail.com>
2024-02-07 23:35:41 +0600

    header z-index issue fixed

--- resources/js/Template/frontend/fragments/topNavigation/Index.vue
+++ resources/js/Template/frontend/fragments/topNavigation/Index.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class="bg-white border-t-4 border-secondary shadow-md px-4 py-2 sticky top-0 z-[9999]">
+    <header class="bg-white border-t-4 border-secondary shadow-md px-4 py-2 sticky top-0 z-[9]">
         <div class="custom-container">
             <nav class="flex justify-between items-center">
                 <LeftContent />
