4a9c1b4c1f1830bebf973f57062627355c3640dc
Khalid-Hossain-Tarif <khalidhossain1311@gmail.com>
2024-02-21 01:56:46 +0600

    wishlist product design updated and mobile responsive done

--- resources/js/Template/frontend/wishlist/Index.vue
+++ resources/js/Template/frontend/wishlist/Index.vue
@@ -2,39 +2,35 @@
     <Layout>
         <div class="py-12">
             <div class="custom-container">
-                <h1 class="mb-7 text-3xl font-semibold">My wishlist products</h1>
-                <div class="bg-white shadow-md rounded">
-                    <div class="grid grid-cols-2 bg-[#ECEDEC] text-center">
-                        <div class="px-4 py-2 font-semibold uppercase text-sm">Product title</div>
-                        <div class="px-4 py-2 font-semibold uppercase text-sm">Print price</div>
-                        <div></div>
-                    </div>
-
-                    <div 
-                        v-for="n in 5"
-                        class="relative"
+                <h1 class="mb-7 text-3xl font-semibold">{{printString('My wishlist products')}}</h1>
+                <div class="space-y-4">
+                    <article 
+                        v-for="n in 7"
                     >
-                        <div class="grid grid-cols-2 text-center border-b">
-                            <h4 class="px-4 py-3 text-sm">
-                                Audi A5 Cabriolet 1.8 TFSI Pro Line S-Line/DAB+/ROTOR/PDC/XE
-                            </h4>
-                            <div class="px-4 py-3 text-sm font-semibold">
-                                {{ printPrice(16.249) }}
+                        <div class="bg-white shadow-md rounded flex justify-between items-center">
+                            <div class="relative flex items-center gap-4">
+                                <Link href="/single-product" class="absolute inset-0 z-50"></Link>
+                                <Img 
+                                    src="https://mp.images.icas.io/api/v1/a6519ad0/images/3f/3f46e442-a09f-4ebd-aa2a-2382d43d6b7a?rule=eps_83"
+                                    alt="Car" 
+                                    class="w-[180px] h-24 lg:h-28"
+                                />
+                                <div>
+                                    <h4 class="text-lg text-primary font-medium ellipsis-text">
+                                        Opel Movano Delivery van L2 H2 2018 Diesel Manua
+                                    </h4>
+                                    <p class="mt-2 font-semibold">{{ printPrice(1600) }}</p>
+                                </div>
+                            </div>
+                            <div class="px-4">
+                                <Icon 
+                                    name="PhXCircle"
+                                    size="20"
+                                    color="red"
+                                />
                             </div>
                         </div>
-                        <div class="absolute right-4 top-1/2 -translate-y-1/2">
-                            <Button.Native
-                                :icon="{
-                                    name: 'PhXCircle',
-                                    size: 20,
-                                    color: 'red'
-                                }"
-                                outline="true"
-                                class="shrink-0"
-                            >
-                            </Button.Native>
-                        </div>
-                    </div>
+                    </article>
                 </div>
             </div>
         </div>
@@ -43,6 +39,7 @@
 
 <script setup>
 import Layout from '@/Template/frontend/layout/Index.vue'
-import { Button } from "@/plugins/ui"
-import { printPrice } from '@/utilities/helper'
+import { Button, Img, Icon } from "@/plugins/ui" 
+import { Link } from '@inertiajs/vue3'
+import { printString, printPrice } from '@/utilities/helper'
 </script>
